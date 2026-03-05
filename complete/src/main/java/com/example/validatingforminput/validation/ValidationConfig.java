16df4814c0d6d3f33d94223572c33c534f63c781
hectorad <alex.delzar@gmail.com>
2026-03-04 22:40:23 -0600

    third pass bean validation

--- complete/src/main/java/com/example/validatingforminput/validation/ValidationConfig.java
+++ complete/src/main/java/com/example/validatingforminput/validation/ValidationConfig.java
@@ -1,13 +1,10 @@
 package com.example.validatingforminput.validation;
 
 import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.springframework.boot.autoconfigure.validation.ValidationConfigurationCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties(ValidationProperties.class)
@@ -20,33 +17,18 @@ public class ValidationConfig {
 	}
 
 	@Bean
-	@Primary
-	public LocalValidatorFactoryBean validator() {
-		LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
-		validatorFactoryBean.setConfigurationInitializer(configuration -> {
-			if (configuration instanceof HibernateValidatorConfiguration hibernateConfiguration) {
-				contributor.createConstraintMappings(() -> {
-					var mapping = hibernateConfiguration.createConstraintMapping();
-					hibernateConfiguration.addMapping(mapping);
-					return mapping;
-				});
-			}
-			else {
-				throw new IllegalStateException(
-					"Hibernate Validator configuration is required for config-driven constraint mapping.");
-			}
-		});
-		validatorFactoryBean.afterPropertiesSet();
-		return validatorFactoryBean;
-	}
+	public ValidationConfigurationCustomizer validationConfigurationCustomizer() {
+		return configuration -> {
+            if (!(configuration instanceof HibernateValidatorConfiguration hibernateConfiguration)) {
+                throw new IllegalStateException(
+                    "Hibernate Validator configuration is required for config-driven constraint mapping.");
+            }
+            contributor.createConstraintMappings(() -> {
+                var mapping = hibernateConfiguration.createConstraintMapping();
+                hibernateConfiguration.addMapping(mapping);
+                return mapping;
+            });
 
-	@Bean
-	public WebMvcConfigurer webMvcValidationConfigurer(LocalValidatorFactoryBean validator) {
-		return new WebMvcConfigurer() {
-			@Override
-			public Validator getValidator() {
-				return validator;
-			}
-		};
+        };
 	}
 }
